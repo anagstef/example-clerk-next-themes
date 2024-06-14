c9807e27a44fd3a3545057d4c030326df3a3473c
Stefanos Anagnostou <stefanos@clerk.dev>
2024-06-14 15:01:10 +0300

    feat: Add Clerk authentication provider to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { ClerkAuthProvider } from "./ClerkAuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-      <ThemeProvider>{children}</ThemeProvider>
+      <ThemeProvider>
+        <ClerkAuthProvider>{children}</ClerkAuthProvider>
+      </ThemeProvider>
       </body>
     </html>
   );
